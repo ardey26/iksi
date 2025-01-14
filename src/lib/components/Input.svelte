6cc61c5deae5c931ab183f7e5367540e8faf7b06
ardey26 <andorekun26@gmail.com>
2025-01-14 12:31:12 +0800

    Fixed UI layout

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -5,35 +5,35 @@
 
 <form on:submit|preventDefault={handleSubmit} class="form-control flex flex-col gap-4">
 	<div class="relative">
-		<label class="label text-white text-3xl"> go ahead, shorten something... </label>
+		<label class="label text-white text-3xl"> go ahead, shorten something </label>
 		<div class="row">
 			<input
 				type="text"
 				name="longURL"
 				id="longURL"
-				placeholder="long url..."
+				placeholder="Paste your long URL here…"
 				bind:value={longURL}
-				class="pl-3 pr-[7rem] w-full h-[3rem] rounded-md border border-gray-300 bg-neutral-900 text-white"
+				class="pl-3 pr-[7rem] w-full h-[3rem] rounded-md border border-gray-300 bg-neutral-900 text-white text-base"
 			/>
 			<div class="absolute bottom-0 right-0">
-				<button type="submit" class="btn btn-outline px-8 border text-white lowercase">
+				<button type="submit" class="btn btn-outline px-8 border-2 bg-neutral-700 text-white lowercase">
 					enter
 				</button>
 			</div>
 		</div>
 	</div>
-	<div class="form-group">
+	<div class="form-group flex flex-col justify-end items-end">
 		<label class="label" for="customURL">
-			<span class="label-text text-white text-lg md:text-2xl mr-2"> wanna make a custom one? </span>
-			<input
-				type="text"
-				name="customURL"
-				id="customURL"
-				bind:value={customURL}
-				maxlength="24"
-				class="pl-3 w-2/5 h-[3rem] rounded-md border border-gray-300 bg-neutral-900 text-white text-sm truncate"
-				placeholder="optional custom link..."
-			/>
+			<span class="label-text text-white text-lg mr-2"> wanna make a custom one? </span>
 		</label>
+		<input
+			type="text"
+			name="customURL"
+			id="customURL"
+			bind:value={customURL}
+			maxlength="24"
+			class="p-2 w-full rounded-md border border-gray-300 bg-neutral-900 text-white truncate text-base"
+			placeholder="Optional custom link..."
+		/>
 	</div>
 </form>
