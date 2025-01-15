442727f71e5a961c84e24eac939ea3e99759c62e
ardey26 <andorekun26@gmail.com>
2025-01-15 11:23:25 +0800

    Only handle get requests

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,7 @@
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-    if (event.url.pathname.startsWith('/api')) {
+    if (event.request.method === 'GET' && event.url.pathname.startsWith('/api')) {
         return new Response(null, {
             status: 302,
             headers: {
