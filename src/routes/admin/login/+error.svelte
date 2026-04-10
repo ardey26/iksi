643f78d919df587f727e5cf5e84bf4e8b7da2d37
ardey26 <andorekun26@gmail.com>
2026-04-10 11:56:09 +0800

    fix: use slug error page for admin routes on main domain

---
+++ src/routes/admin/login/+error.svelte
@@ -0,0 +1,9 @@
+<div class="hero h-[93.5vh]">
+	<div class="hero-content text-center border border-4 p-40">
+		<div class="max-w-md">
+			<h1 class="text-5xl font-bold">ERROR 404 NOT FOUND</h1>
+			<p class="py-6">your short url isn't pointing anywhere, claim it if you want.</p>
+			<a href="/"><button class="btn btn-outline lowercase">go back</button></a>
+		</div>
+	</div>
+</div>
