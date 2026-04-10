c44b8bbd4426386fe6273ad445a54a8f2661e1b3
ardey26 <andorekun26@gmail.com>
2026-04-10 16:59:25 +0800

    feat(auth): add AuthDropdown and UserMenu components

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -2,3 +2,5 @@ export { default as Input } from './Input.svelte';
 export { default as ShortenedURL } from './ShortenedURL.svelte';
 export { default as ThemeToggle } from './ThemeToggle.svelte';
 export { default as SEO } from './SEO.svelte';
+export { default as AuthDropdown } from './AuthDropdown.svelte';
+export { default as UserMenu } from './UserMenu.svelte';
