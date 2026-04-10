1899f1d9018c3fa65a5c88c7240703fb81c25848
ardey26 <andorekun26@gmail.com>
2026-04-10 11:05:20 +0800

    fix: update logout to clear admin_session cookie

--- src/routes/admin/logout/+server.js
+++ src/routes/admin/logout/+server.js
@@ -1,6 +1,6 @@
 import { json } from '@sveltejs/kit';
 
 export const POST = async ({ cookies }) => {
-	cookies.delete('admin_authenticated', { path: '/' });
+	cookies.delete('admin_session', { path: '/' });
 	return json({ success: true });
 };
