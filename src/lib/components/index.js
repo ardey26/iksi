19c11a43f725c56d6c322f080126273322eae5b7
ardey26 <andorekun26@gmail.com>
2026-04-11 12:59:45 +0800

    fix: restore Footer and add Header/Footer exports

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -4,3 +4,5 @@ export { default as ThemeToggle } from './ThemeToggle.svelte';
 export { default as SEO } from './SEO.svelte';
 export { default as AuthDropdown } from './AuthDropdown.svelte';
 export { default as UserMenu } from './UserMenu.svelte';
+export { default as Header } from './Header.svelte';
+export { default as Footer } from './Footer.svelte';
